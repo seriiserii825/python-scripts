d8fc00d47b31efcab712ce9de23379ff19d53acb
serii burduja <seriiburduja@gmail.com>
2025-01-04 22:56:28 +0200

    feat: zip-folders

---
+++ zip-folders.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import os
+folders = [f for f in os.listdir('.') if os.path.isdir(f)]
+for index, folder in enumerate(folders):
+    print(f'{index + 1}. {folder}')
+    os.system(f'zip -r "{folder}.zip" "{folder}"')
